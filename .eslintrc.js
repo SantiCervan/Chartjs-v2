d399b33b5b5b2b5db59f2f6aff6d0f200ef07a9c
santiago <frontend@arcoch.com>
2024-08-29 10:44:32 -0300

    Add: eslint, prettier, precommit and pre-push with husky

---
+++ .eslintrc.js
@@ -0,0 +1,10 @@
+/* eslint-disable prettier/prettier */
+module.exports = {
+  extends: ['next/core-web-vitals', 'prettier'],
+  plugins: ['prettier'],
+  rules: {
+    'prettier/prettier': 'error',
+    'no-unused-vars': 'warn',
+    'no-console': 'warn',
+  },
+};
